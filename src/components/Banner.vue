999d5e1b8515c2e21d4179458ca9507cf0531c6f
Eduardo Nowakoski <eduardo_nowa@hotmail.com>
2022-06-11 01:26:08 -0300

    Feat: Make your burguer vue.js

---
+++ src/components/Banner.vue
@@ -0,0 +1,46 @@
+<template>
+    
+    <div id="main-banner">
+        <h1>Make Your Burguer</h1>
+    </div>
+
+</template>
+
+
+
+<script>
+export default {
+    name: "Banner"
+}
+</script>
+
+
+
+
+<style scoped>
+
+#main-banner {
+    background-image: url('/public/img/burger.jpg');
+    background-position: 100%;
+    background-size: cover;
+    height: 40vh;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+}
+
+#main-banner h1 {
+    color: #fff;
+    text-align: center;
+    font-size: 3em;
+    background-color: #222;
+    padding: 20px 40px;
+}
+
+/* @media (max-width:1061px){
+    #main-banner {
+        background-position:right bottom
+    }
+} */
+</style>
+
